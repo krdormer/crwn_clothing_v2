d9a5861cc9c7548097ce12d2e80f7f9a864bc9c9
Kevin Dormer <krdormer_dev@protonmail.com>
2022-04-09 19:42:58 -0500

    Refactor App to dynamically generate content

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+const App = () => {
+  const categories = [
+    { id: 1, title: 'HATS' },
+    { id: 2, title: 'JACKETS' },
+    { id: 3, title: 'SNEAKERS' },
+    { id: 4, title: 'WOMENS' },
+    { id: 5, title: 'MENS' },
+  ]
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="categories-container">
+      {categories.map(({ title }) => (
+        <div className="category-container">
+          <div className="background-image"/>
+          <div className="category-body-container">
+            <h2>{title}</h2>
+            <p>SHOP NOW</p>
+          </div>
+        </div>
+      ))}
     </div>
-  );
+  )
 }
 
 export default App;
