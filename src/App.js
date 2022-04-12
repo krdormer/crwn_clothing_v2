079c47302375fdebb57e73406226c044f93bc1a1
Kevin Dormer <krdormer_dev@protonmail.com>
2022-04-12 18:36:50 -0500

    Add react-router, home route

--- src/App.js
+++ src/App.js
@@ -1,16 +1,26 @@
+// Routing
+import { Routes, Route, Outlet } from 'react-router-dom';
 // Components
-import Directory from './components/directory/directory';
+import Home from './routes/Home/Home';
+
+const Navigation = () => {
+  return (
+    <div>
+      <div>
+        <h1>I AM THE NAVBAR</h1>
+      </div>
+      <Outlet />
+    </div>
+  )
+}
 
 const App = () => {
-  const categories = [
-    { id:	1, title:	"hats", imageUrl:	"https://i.ibb.co/cvpntL1/hats.png" },
-    { id:	2, title:	"jackets", imageUrl:	"https://i.ibb.co/px2tCc3/jackets.png" },
-    { id:	3, title:	"sneakers", imageUrl:	"https://i.ibb.co/0jqHpnp/sneakers.png" },
-    { id:	4, title:	"womens", imageUrl:	"https://i.ibb.co/GCCdy8t/womens.png" },
-    { id:	5, title:	"mens", imageUrl:	"https://i.ibb.co/R70vBrQ/men.png"}
-  ]
   return (
-    <Directory categories={categories} />
+    <Routes>
+      <Route path='/' element={<Navigation />}>
+        <Route index element={<Home />} />
+      </Route>
+    </Routes>
   )
 }
 
