681ec5d5c81e96d9dd3a00ff66466cddeda66249
Kevin Dormer <kevin.dormer@favordelivery.com>
2022-05-17 16:39:26 -0500

    Remove template code, setup firebase

--- src/App.js
+++ src/App.js
@@ -3,12 +3,23 @@ import { Routes, Route } from 'react-router-dom';
 // Components
 import Navigation from './routes/Navigation/Navigation';
 import Home from './routes/Home/Home';
+import SignIn from './routes/sign-in/sign-in';
+
+const Shop = () => {
+  return (
+    <div>
+      <h1>Shop Page</h1>
+    </div>
+  )
+}
 
 const App = () => {
   return (
     <Routes>
       <Route path='/' element={<Navigation />}>
         <Route index element={<Home />} />
+        <Route path='shop' element={<Shop />} />
+        <Route path='signin' element={<SignIn />} />
       </Route>
     </Routes>
   )
--- src/routes/Navigation/Navigation.jsx
+++ src/routes/Navigation/Navigation.jsx
@@ -11,6 +11,8 @@ const Navigation = () => {
           <CrwnLogo className="logo" />
         </Link>
         <div className="nav-links-container">
+          <Link className="nav-link" to='/shop'>SHOP</Link>
+          <Link className="nav-link" to='/signin'>SIGN IN</Link>
         </div>
       </header>
       <Outlet />
